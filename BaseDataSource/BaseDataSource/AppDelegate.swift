3420b5a4ab1495fd19e33244cba772b72b80d52f
Ivan Rep <ivan.rep2@gmail.com>
2018-10-26 15:37:31 +0200

    Base data source project

---
+++ BaseDataSource/BaseDataSource/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow? = UIWindow()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let rootViewController = UINavigationController(rootViewController: ViewController(nibName: nil, bundle: nil))
+        window?.rootViewController = rootViewController
+
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
